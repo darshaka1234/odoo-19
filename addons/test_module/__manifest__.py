097c3dea9d0ce1f1927c50828f203fada311b868
darshaka <darshakasri@example.com>
2025-11-13 02:05:22 +0530

    test module one

---
+++ addons/test_module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'My Custom 6',
+    'version': '1.0',
+    'author': 'Your Name',
+    'website': 'https://yourwebsite.com',
+    'category': 'Sales',
+    'summary': 'A simple custom module example',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/my_model_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
